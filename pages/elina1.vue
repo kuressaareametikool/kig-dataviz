76b2e4c8b997c213a35f5741ceb6d04bad625f63
Elina Aru <92989103+elinaametikool@users.noreply.github.com>
2022-11-02 15:29:05 +0200

    elina_katse2

--- pages/elina1.vue
+++ pages/elina1.vue
@@ -1,17 +1,22 @@
 <script setup>
-//    0 1 2 3 4 5 6
-//     E T K N R L P
-const data = [5, 6, 5, 7, 9, 6, 5];
+// i          0  1  2  3  4  5  6
+//            E  T  K  N  R  L  P
+const data = [5, 3, 2, 3, 1, 3, 5];
 </script>
 
 <template>
   <div>
-    <pre v-for="d in data" :style="{ opacity: 0.1 + d / 3 }">{{ d }}</pre>
+    <pre v-for="(d, i) in data" :style="{ opacity: 0.1 + d / 3 }">{{ d }}</pre>
     <svg width="500" height="500" class="bg-gray-100">
-      <rect x="50" y="50" width="50" height="50" opacity="0.5" />
-      <rect x="100" y="50" width="50" height="50" opacity="0.2" />
-      <rect x="150" y="50" width="50" height="50" opacity="0.1" />
-      <rect x="200" y="50" width="50" height="50" opacity="0.05" />
+      <circle
+        v-for="(d, i) in data"
+        :style="{ opacity: 0.1 + d / 3, fill: 'red' }"
+        :cx="i * 50 + 50"
+        cy="50"
+        :r="d * 10"
+        width="50"
+        height="50"
+      />
     </svg>
   </div>
 </template>
