8ea8233c21532452dba223956e4d6f2de05d3b78
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-11-02 15:40:34 +0200

    mjah

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<script setup>
-const { data } = await useSheet(
-  "1rq0SknbfsfC3fcxt_vMMX_-KQX8hNmAGF98OB7r3PSU",
-  "värvidkokku"
-);
-</script>
- 
-<template>
-  <div v-for="col in range(1, 8)" class="pt-12">
-    {{ data[0][col] }}
-     <p
-      v-for="row in range(1, 12)"
-    :style="{ 
-        width: data[row][col] * 50 + 'px',
-        background: data[row][0],
-        color: 'darkgray',
-        padding: '5px 10px',
-    }"
-    >
-      {{ data[row][0].slice(0, 10) }}
-    </p>
-  </div>
-=======
 <template>
   <pre>Esileht</pre>
->>>>>>> 5dd40d7b2d0aaac0c591618fab14f80e75cdbb35
 </template>
