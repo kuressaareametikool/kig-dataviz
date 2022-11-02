b8d703d6f4115dbbfe56504b52538b44a487040f
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-11-02 13:28:45 +0200

    koood

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,18 @@ const { data } = await useSheet(
 </script>
 
 <template>
-  <div>
-    <pre>{{ data }}</pre>
+  <div v-for="col in range(1, 8)" class="pt-12">
+    {{ data[0][col] }}
+    <p
+      v-for="row in range(1, 12)"
+      :style="{
+        width: data[row][col] * 50 + 'px',
+        background: data[row][0],
+        color: 'darkgray',
+        padding: '5px 10px',
+      }"
+    >
+      {{ data[row][0].slice(0, 10) }}
+    </p>
   </div>
 </template>
