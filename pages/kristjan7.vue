77024e2dff7dfe35aec023af5b88eacae5fb1d3f
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-11-03 13:20:15 +0200

    hm

--- pages/kristjan7.vue
+++ pages/kristjan7.vue
@@ -15,31 +15,36 @@ const { data: colors } = await useSheet(
 function transpose(matrix) {
   return matrix[0].map((col, i) => matrix.map((row) => row[i]));
 }
+
+const toNumber = (num) =>
+  Number.isNaN(parseFloat(num)) ? num : parseFloat(num);
+
+const d = computed(() => transpose(colors.value).map((a) => a.map(toNumber)));
 </script>
 
 <template>
   <div>
-    <pre>{{}}</pre>
+    <!-- <pre>{{ d }}</pre> -->
     <svg width="500" height="750" xmlns="http://www.w3.org/2000/svg">
-      <rect width="500" height="750" fill="#eee" />
-      <g v-for="(cols, row) in transpose(colors).slice(0, -1)">
-        <g v-for="(value, col) in cols.slice(0, -1)">
-          <text :x="(col - 1) * 50 + 100" :y="row * 50 + 100">
-            {{ value }}
-          </text>
-          <!-- <circle
-            v-if="col > 0 && parseInt(value) > 0"
-            :style="{
-              opacity: 0.8,
-              fill: cols[0],
-            }"
-            :cx="(col - 1) * 50 + 100"
-            :cy="(row - 1) * 50 + 100"
-            :r="area2radius(parseInt(value)) * 15 + 2"
-          /> -->
+      <rect width="500" height="750" fill="#444" />
+      <g v-for="(cols, col) in d.slice(0, -1)">
+        <!-- <g v-for="(value, col) in cols.slice(0, -1)"> -->
+        <!-- <text :x="(col - 1) * 50 + 100" :y="0 * 50 + 100">
+          {{ cols[0] }}
+        </text> -->
+        <g v-for="(value, row) in cols.slice(0, -1)">
+          <rect
+            v-for="count in range(0, value - 1)"
+            :x="(col - 1) * 50 + 80"
+            :y="row * 45 + 50 + count * 3"
+            width="30"
+            height="2"
+            :style="{ fill: d[0][row] }"
+          />
         </g>
+        <!-- </g> -->
       </g>
-      <text x="200" y="680" opacity="0.4" fontFamily="sans-serif">
+      <text x="200" y="680" fill="white" opacity="0.4" fontFamily="sans-serif">
         E T K N R L P
       </text>
     </svg>
